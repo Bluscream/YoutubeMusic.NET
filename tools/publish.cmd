d48b28717d965f008f4ab4a38e4952774db10cd0
Bluscream <Bluscream@users.noreply.github.com>
2026-02-02 00:52:30 +0100

    Build at 2026-02-02 00:52:30

--- tools/publish.cmd
+++ tools/publish.cmd
@@ -1,1 +1,1 @@
-powershell -ExecutionPolicy Bypass -Command "build.ps1" -build -publish -git -Github -Release -Debug -csproj StreamingPlayerNET.csproj -sln StreamingPlayerNET.sln -repo StreamingPlayerNET %*
+powershell -ExecutionPolicy Bypass -Command "build.ps1" -build -publish -git -Github -Release -Debug -csproj ytmNET.csproj -sln ytmNET.sln -repo ytm-ui-dotnet %*
